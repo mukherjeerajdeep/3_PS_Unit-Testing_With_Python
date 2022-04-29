bcb8958b351ecf12de7c5b78da7df8aed2a797cc
Rajdeep Mukherjee <rajdeep.mukherjee@ericsson.com>
2022-04-29 16:10:13 +0200

    unittest.skip

--- 02/phone_numbers/phonebook.py
+++ 02/phone_numbers/phonebook.py
@@ -8,5 +8,20 @@ class PhoneBook:
     def lookup(self, name):
         return self.numbers[name]
 
+    # {
+    #   "Bob": 12345,
+    #   "Anna": 45678,
+    #   "Tom": 123
+    #  }
+    #
+    #  So Bob will be compared with Bob,Anna,Tom and then Anna will be with Bob,Anna,Tom.
+    #  If name matches we continue because we are not checking names but if numbers first
+    #  few digits matches with the other number then it should fail.
     def is_consistent(self):
-        pass
+        for name1, number1 in self.numbers.items():
+            for name2, number2 in self.numbers.items():
+                if name1 == name2:
+                    continue
+                if number1.startswith(number2):
+                    return False
+        return True
--- 02/phone_numbers/test_phonebook.py
+++ 02/phone_numbers/test_phonebook.py
@@ -5,6 +5,7 @@ from phonebook import PhoneBook
 
 class PhoneBookTest(unittest.TestCase):
 
+    # Setup and Teardown will run for each test cases
     def setUp(self) -> None:
         self.phonebook = PhoneBook()
 
@@ -24,6 +25,37 @@ class PhoneBookTest(unittest.TestCase):
         with self.assertRaises(KeyError):
             self.phonebook.lookup("missing")
 
-    @unittest.skip("WIP")
+    # @unittest.skip("WIP")
     def test_empty_phonebook_is_consistent(self):
         self.assertTrue(self.phonebook.is_consistent())
+
+    # Bad test case writing where all the tests run together, and the
+    # last one might not get executed because of the previous one is failed.
+    # def test_is_consistent(self):
+    #     self.phonebook.add("Bob", "12345")
+    #     self.assertTrue(self.phonebook.is_consistent())
+    #     self.phonebook.add("Anna", "43124")
+    #     self.assertTrue(self.phonebook.is_consistent())
+    #     self.phonebook.add("Tom", "12345")  # Identical entry to Bob
+    #     self.assertTrue(self.phonebook.is_consistent())
+    #     self.phonebook.add("Tom", "123")  # prefix of Bob
+    #     self.assertTrue(self.phonebook.is_consistent())
+
+    def test_is_consistent_with_different_entries(self):
+        # given -> arrange
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Anna", "43124")
+        # when -> act
+        consistent = self.phonebook.is_consistent()
+        # then -> assert
+        self.assertTrue(consistent)
+
+    def test_inconsistent_with_duplicate_entries(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Tom", "12345")  # Identical entry to Bob
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_inconsistent_with_duplicate_prefix(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Tom", "123")  # prefix of Bob
+        self.assertFalse(self.phonebook.is_consistent())
